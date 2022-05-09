be620e22fd8c83916b0298df82ba97e132c06b1b
Marcos Paulo <marcosazevedo2112@gmail.com>
2022-05-09 16:42:15 -0300

    correções quase prontas

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,10 +1,13 @@
 function verifyScroll(){
   const scroll = window.scrollY;
-  const headerLogo =   document.querySelector('header .desktop .logo');
+  const headerLogo =   document.querySelector('header .desktop .logo-header');
+  const headerBar = document.querySelector('header .desktop');
   if(scroll>=200){
   headerLogo.classList.add('active')
+  headerBar.classList.add('active')
   }else{
     headerLogo.classList.remove('active')
+    headerBar.classList.remove('active')
   }
 }
 
